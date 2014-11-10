20f85496ec4a62f2098744b7709054a3024aaca1
Yigit Can Arin <yigitcan@microsoft.com>
2014-11-10 22:02:02 +0100

    new project

---
+++ Duhigg/Duhigg/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Duhigg
+//
+//  Created by Yigit Can Arin on 11/10/14.
+//  Copyright (c) 2014 Yigit Can Arin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
